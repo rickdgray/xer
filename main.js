f64af58c15e0bf677785bf7a2e14f15b87e2347f
Rick Gray <Rick.Gray@INEIGHT.COM>
2021-03-19 19:24:03 -0500

    grab data from reader and set up parse method

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 "use strict";
-let getFile = () => {
+const parseData = (data) => {
+    console.log(data);
+};
+const getFile = () => {
     var _a;
     const selectedFile = (_a = document.getElementById("fileInput").files) === null || _a === void 0 ? void 0 : _a[0];
     if (selectedFile && selectedFile.type === "application/xer") {
         const reader = new FileReader();
         reader.onload = (event) => {
-            console.log(event.target);
+            if (event.target) {
+                parseData(event.target.result);
+            }
         };
         reader.readAsText(selectedFile);
     }
--- source/main.ts
+++ source/main.ts
@@ -1,10 +1,16 @@
-let getFile = (): void => {
+const parseData = (data: string) => {
+    console.log(data);
+}
+
+const getFile = (): void => {
     const selectedFile = (document.getElementById("fileInput") as HTMLInputElement).files?.[0];
 
     if (selectedFile && selectedFile.type === "application/xer") {
         const reader = new FileReader();
-        reader.onload = (event: Event): void => {
-            console.log(event.target);
+        reader.onload = (event: ProgressEvent<FileReader>): void => {
+            if (event.target) {
+                parseData(event.target.result as string);
+            }
         };
         reader.readAsText(selectedFile);
     } else {
